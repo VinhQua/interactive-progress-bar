603d43715d79f4029d8848c565983e8e2743a82b
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-03-30 12:53:18 +0900

    fix width formula

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,12 +1,10 @@
 import { useRef } from "react"
 
 const ProgressBar =(props)=>{
-        const bar = document.querySelector('.progress-bar');
-        const percentageWidth = (bar.clientWidth/100)*props.userInput
         const value = props.userInput || 0;
     return(
         <div  className="progress-bar">
-            <div className="progress-fill" style={{width:percentageWidth}}>
+            <div className="progress-fill" style={{width:`${value}%`}}>
                 <span className="progress-text">{value}%</span>
             </div>
         </div>
